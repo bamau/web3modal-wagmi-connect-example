66da760ecdcd4247a71fb6560f64c9385f68ebcb
BaMau <bamau97@gmail.com>
2024-05-06 15:52:15 +0700

    style: update style text

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
   const { open } = useWeb3Modal();
   const { disconnect } = useDisconnect();
   const { switchChain } = useSwitchChain();
-  
-  console.log('Log - chainId:', chainId)
-  console.log('Log - Number(chainId) !== 43113:', Number(chainId) !== 43113)
+
+  console.log("Log - chainId:", chainId);
+  console.log("Log - Number(chainId) !== 43113:", Number(chainId) !== 43113);
   return (
-    <div className="bg-black">
-      <h1>Vite + React</h1>
-      <div className='card'>
+    <div className='bg-black'>
+      <h1 className='text-white'>Vite + React</h1>
+      <div className='card text-white'>
         <button className='mr-4 mb-4' onClick={() => (address ? disconnect() : open())}>
           {address ? `${address.slice(0, 6)}...${address.slice(-4)}` : "Connect Wallet"}
         </button>
@@ -22,11 +22,7 @@ function App() {
           {" "}
           {Number(chainId) !== 43113 ? "Switch Chain" : "Correct chain"}{" "}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
