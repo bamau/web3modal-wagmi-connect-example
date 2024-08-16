f5adbb8ff579530c216a5434589052a16732d86e
BaMau <bamau97@gmail.com>
2024-08-16 17:47:20 +0700

    chore: add switch chain network

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-// 
-
+//
 
 import { useSDK } from "@metamask/sdk-react";
 import { useState } from "react";
@@ -7,13 +6,13 @@ import { useState } from "react";
 export const App = () => {
   const [account, setAccount] = useState<string>();
   const { sdk, connected, provider, chainId } = useSDK();
-  console.log('Log - chainId:', chainId)
-  console.log('Log - provider:', provider)
+  console.log("Log - chainId:", chainId);
+  console.log("Log - provider:", provider);
 
   const connect = async () => {
     try {
       const accounts = await sdk?.connect();
-      console.log('Log - accounts:', accounts)
+      console.log("Log - accounts:", accounts);
       setAccount(accounts?.[0]);
     } catch (err) {
       console.warn("failed to connect..", err);
@@ -28,14 +27,32 @@ export const App = () => {
     }
   };
 
+  const changeNetwork = async (hexChainId: string) => {
+    console.debug(`switching to network chainId=${hexChainId}`);
+    try {
+      const response = await provider?.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: hexChainId }], // chainId must be in hexadecimal numbers
+      });
+      console.debug(`response`, response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
       <button style={{ padding: 10, margin: 10 }} onClick={connect}>
         Connect
       </button>
       <button style={{ padding: 10, margin: 10 }} onClick={disconnect}>
         Disconnect
       </button>
+      {Number(chainId) !== 545 && (
+        <button style={{ padding: 10, margin: 10 }} onClick={() => changeNetwork("0x221")}>
+          Switch to FLOW network
+        </button>
+      )}
       {connected && (
         <div>
           <>
